d85b0cc93b332e0936458f7dcfe128085143b20a
Leiva, Emiliano <emilianoleiva20j@gmail.com>
2025-10-27 18:57:23 -0300

    Subo proyecto inicial (src, public, config)

---
+++ react-tienda-tp-final-main/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css'; 
+import { useAuthContext } from '../context/AuthContext';
+// Se pide para la pre-entrega
+const Navbar = () => {
+  const { usuario } = useAuthContext();
+  const esAdmin = usuario === 'admin';
+  
+  return(
+    <nav>
+      <ul className={styles.lista}>
+        <li className={styles.item}>
+          <Link to="/" className={styles.link}>Inicio</Link>
+          <Link to="/tecnologia" className={styles.link}>Tecnologia</Link>
+          <Link to="/moda" className={styles.link}>Moda</Link>
+          {esAdmin && 
+            <Link to="/admin" className={styles.link}>Admin</Link>
+          }
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+
